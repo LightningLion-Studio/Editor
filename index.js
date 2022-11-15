e32a785a0703a310ce76cef47356035384ec1bde
groupguanfang <1203970284@qq.com>
2022-11-15 16:37:28 +0800

    6

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3013
+
+app.use('/',require('./src/index'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
