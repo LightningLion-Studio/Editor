096c2ebc8f06d7341ee486dcef30cdc727f40c57
groupguanfang <1203970284@qq.com>
2022-11-15 21:44:46 +0800

    origin

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const port = 3013
 
 // 跨域设置
 app.use('*', function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  // res.setHeader("Access-Control-Allow-Origin", req.get("Origin")); // 添加这一行代码，代理配置不成功
-  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, If-Modified-Since')
-  next()
+  // 设置允许跨域的域名,*代表允许任意域名跨域
+  res.header('Access-Control-Allow-Origin', '*')
+  // 允许的header类型
+  res.header('Access-Control-Allow-Headers', 'content-type')
+  // 跨域允许的请求方式
+  res.header('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS')
+  if (req.method.toLowerCase() == 'options') {
+    res.send(200)
+  } else {
+    next()
+  }
 })
 
 app.use('/', require('./src/index'))
