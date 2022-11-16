b531e1e4b09e27a8edc4b9fa5c2888110d3c4d91
groupguanfang <1203970284@qq.com>
2022-11-16 21:01:53 +0800

    6

--- src/components/examples/index.vue
+++ src/components/examples/index.vue
@@ -2,17 +2,12 @@
   <div class="example">
     <toolbar
       :config="config"
-      :disabled="loading"
       :themes="Object.keys(themes)"
       :languages="Object.keys(languages)"
       @language="ensureLanguageCode"
     />
     <div class="divider"></div>
-    <div class="loading-box" v-if="loading">
-      <loading />
-    </div>
     <editor
-      v-else-if="currentLangCode"
       :config="config"
       :theme="currentTheme"
       :language="currentLangCode.language"
@@ -23,10 +18,8 @@
 
 <script lang="ts">
   import { defineComponent, reactive, computed, shallowRef, onBeforeMount } from 'vue'
-  import { Theme, useTheme } from '@/composables/theme'
-  import Loading from '@/components/common/loading.vue'
   import languages from './languages'
-  import themes from './themes'
+  import { oneDark } from "@codemirror/theme-one-dark";
   import Toolbar from './toolbar.vue'
   import Editor from './editor.vue'
 
@@ -35,7 +28,6 @@
     title: 'Example Source Code',
     url: import.meta.url,
     components: {
-      Loading,
       Toolbar,
       Editor
     },
@@ -47,10 +39,9 @@
         autofocus: true,
         height: 'auto',
         language: 'javascript',
-        theme: useTheme().theme.value === Theme.Light ? 'default' : 'oneDark'
+        theme: oneDark
       })
 
-      const loading = shallowRef(false)
       const langCodeMap = reactive(new Map<string, { code: string; language: () => any }>())
       const currentLangCode = computed(() => langCodeMap.get(config.language)!)
       const currentTheme = computed(() => {
@@ -59,7 +50,6 @@
 
       const ensureLanguageCode = async (targetLanguage: string) => {
         config.language = targetLanguage
-        loading.value = true
         const delayPromise = () => new Promise((resolve) => window.setTimeout(resolve, 260))
         if (langCodeMap.has(targetLanguage)) {
           await delayPromise()
@@ -67,18 +57,15 @@
           const [result] = await Promise.all([languages[targetLanguage](), delayPromise()])
           langCodeMap.set(targetLanguage, result.default)
         }
-        loading.value = false
       }
 
       // HACK: Make sure the first screen the user sees is the loading placeholder
-      loading.value = true
       onBeforeMount(() => {
         // init default language & code
         ensureLanguageCode(config.language)
       })
 
       return {
-        loading,
         config,
         themes,
         languages,
@@ -89,25 +76,3 @@
     }
   })
 </script>
-
-<style lang="scss" scoped>
-  .example {
-    .divider {
-      height: 1px;
-      background-color: $border-color;
-    }
-
-    .loading-box {
-      width: 100%;
-      min-height: 20rem;
-      max-height: 60rem;
-      /* loading height = view-height - layout-height - page-height */
-      /* navbar + banner + footer */
-      $layout-height: $navbar-height + $banner-height + $footbar-height;
-      /* single-card-gap * 2 + card-header + editor-header */
-      $page-height: 2rem * 2 + 3.2rem + 3rem;
-      /* editor-border * 2 */
-      height: calc(100vh - $layout-height - $page-height - 2px);
-    }
-  }
-</style>
