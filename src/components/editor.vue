0487c14663b3dde52d9954548f9f3849cb3e2ea8
groupguanfang <1203970284@qq.com>
2022-11-17 20:40:20 +0800

    6

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -9,7 +9,7 @@
   />
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import Codemirror from "codemirror-editor-vue3";
 
 // language
@@ -18,24 +18,16 @@ import "codemirror/mode/htmlmixed/htmlmixed.js"
 import "codemirror/theme/monokai.css"
 
 import { ref } from "vue";
-export default {
-  components: { Codemirror },
-  setup() {
-    const code = ref(``)
-    let cmOptions = {
-        mode: "javascript", // 语言模式
-        theme: "monokai", // 主题
-        lineNumbers: true, // 显示行号
-        smartIndent: true, // 智能缩进
-        indentUnit: 2, // 智能缩进单位为4个空格长度
-        foldGutter: true, // 启用行槽中的代码折叠
-        styleActiveLine: true, // 显示选中行的样式
-      };
-    return {
-      code, cmOptions
-    }
-  },
-};
+const code = ref(``)
+let cmOptions = {
+    mode: "javascript", // 语言模式
+    theme: "monokai", // 主题
+    lineNumbers: true, // 显示行号
+    smartIndent: true, // 智能缩进
+    indentUnit: 2, // 智能缩进单位为4个空格长度
+    foldGutter: true, // 启用行槽中的代码折叠
+    styleActiveLine: true, // 显示选中行的样式
+}
 </script>
 
 <style lang="less">
